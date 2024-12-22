e1a79da6867c9c341122c2f27c56482c2a514e62
Kory <koriellopez03@gmail.com>
2024-12-21 20:31:16 -0600

    include tailwindcss, started navbar, theme fixed

---
+++ components/NavBar.tsx
@@ -0,0 +1,10 @@
+export default function NavBar() {
+  return (
+    <div className="fixed flex flex-col w-32 h-full rounded-r-lg bg-black space-y-5 pt-5">
+      <button>About Me</button>
+      <button>Experience</button>
+      <button>Projects</button>
+      <button>Contact Me</button>
+    </div>
+  );
+}
