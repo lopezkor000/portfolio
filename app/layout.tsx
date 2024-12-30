38c2e3430f25f6fd91484858a502cbb1d458650e
Kory <74568018+lopezkor000@users.noreply.github.com>
2024-12-29 22:11:54 -0600

    added logo to tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.svg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-zinc-900`}
       >
