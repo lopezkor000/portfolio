6661214ccfbe39c250062a13507b64f0cad5da25
Kory <74568018+lopezkor000@users.noreply.github.com>
2025-03-20 19:05:25 -0500

    refactored data output

--- scripts/cleanData.py
+++ scripts/cleanData.py
@@ -6,17 +6,27 @@ with open('data/experiences.html', 'r') as file:
 
 exp = soup.find_all('li', attrs={'class': ["pvs-list__paged-list-item","artdeco-list__item","pvs-list__item--line-separated","pvs-list__item--one-column"]})
 
-with open('data/experiences.json', 'w') as file:
-  obj = {}
+obj = {}
 
-  for i, item in enumerate(exp):
-    obj[i] = []
+for i, item in enumerate(exp):
+  obj[i] = []
 
-    attr = item.find_all("span", attrs={"aria-hidden": "true"})
+  attr = item.find_all("span", attrs={"aria-hidden": "true"})
 
-    for text in attr:
-      obj[i].extend(text.stripped_strings)
-      if "\u00b7" in obj[i][-1]:
-        obj[i][-1] = obj[i][-1].split(" \u00b7 ")
+  for text in attr:
+    obj[i].extend(text.stripped_strings)
+    if "\u00b7" in obj[i][-1]:
+      obj[i][-1] = obj[i][-1].split(" \u00b7 ")
   
+  labeled = {}
+  labeled["position"] = obj[i][0]
+  labeled["company"] = obj[i][1]
+  labeled["when"] = obj[i][2]
+  labeled["where"] = obj[i][3]
+  labeled["description"] = obj[i][4:-1]
+  labeled["skills"] = obj[i][-1]
+
+  obj[i] = labeled
+
+with open('data/experiences.json', 'w') as file:
   json.dump(obj, file, indent=4)
