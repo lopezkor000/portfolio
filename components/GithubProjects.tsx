8c5a0a687adb766d25e15df89c70f2bee2a0dd53
Kory <koriellopez03@gmail.com>
2025-03-24 15:04:46 -0500

    fixed splice error and updated styling

--- components/GithubProjects.tsx
+++ components/GithubProjects.tsx
@@ -3,41 +3,56 @@
 import { useEffect, useState } from "react";
 
 export function GithubProjects() {
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState({ status: 200, data: [] });
 
   useEffect(() => {
     fetch("/api/projects", {
       method: "GET",
     }).then((response) => {
       response.json().then((data) => {
-        setProjects(data.splice(0, 5));
+        // if (data.status != 200) return setProjects(data.status);
+        console.log(data.status);
+        setProjects({
+          status: Number(data.status),
+          data: data.projects.slice(0, 5),
+        });
       });
     });
   }, []);
 
+  // console.log(projects);
+
   return (
     <div>
-      <p>5 Recent GitHub Projects</p>
+      <p className="text-xl font-bold">5 Recently Pushed GitHub Projects</p>
       <ul className="w-fit">
-        {projects.map((project: any) => {
-          return (
-            <li
-              key={project.title}
-              className="border border-yellow-500 border-solid rounded-sm px-5"
-            >
-              <p>
-                <a
-                  href={project.url}
-                  target="_blank"
-                  className="hover:text-yellow-300"
-                >
-                  {project.title}
-                </a>
-              </p>
-              <p>{project.description}</p>
-            </li>
-          );
-        })}
+        {projects.status == 200 ? (
+          projects.data.map((project: any) => {
+            return (
+              <li
+                key={project.title}
+                className="border border-yellow-500 border-solid rounded-sm px-5 py-1"
+              >
+                <p>
+                  <a
+                    href={project.url}
+                    target="_blank"
+                    className="text-xl font-bold text-yellow-300 hover:text-white"
+                  >
+                    {project.title}
+                  </a>
+                </p>
+                {project.description ? (
+                  <p>{project.description}</p>
+                ) : (
+                  <p className="text-zinc-500">No Description</p>
+                )}
+              </li>
+            );
+          })
+        ) : (
+          <div className="text-red-400">Refresh or try again later 😥</div>
+        )}
       </ul>
     </div>
   );
