5a0c8192a8bb96a55bc5691de0a018b1ed0230a7
Kory <74568018+lopezkor000@users.noreply.github.com>
2025-07-06 17:03:19 -0500

    mobile navigation!

---
+++ components/MobileNav.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+} from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+interface ButtonProps {
+  text: string;
+  setOpen: Function;
+}
+
+function MobileNavButton(props: ButtonProps) {
+  const scrollToContent = (id: string) => {
+    const node = document.getElementById(id);
+    window.scroll({ top: node ? node.offsetTop - 100 : 0, behavior: "smooth" });
+  };
+
+  return (
+    <button
+      className="text-yellow-300 bg-zinc-800 w-64 h-12 rounded hover:text-white"
+      onClick={() => {
+        props.setOpen(false);
+        const start = location.href.indexOf("/", 7);
+        if (
+          location.href.slice(start) !== "/" &&
+          location.href.slice(start, start + 2) !== "/#"
+        )
+          location.href = `/#${props.text}`;
+        else scrollToContent(props.text);
+      }}
+    >
+      {props.text}
+    </button>
+  );
+}
+
+interface Props {
+  navs: string[];
+}
+
+export default function MobileNav(props: Props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        onClick={() => setOpen(true)}
+        className="rounded-md px-2.5 py-1.5 text-white"
+      >
+        <Bars3Icon className="text-white size-10" />
+      </button>
+      <Dialog open={open} onClose={setOpen} className="relative z-10">
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-gray-500/75 transition-opacity duration-500 ease-in-out data-closed:opacity-0"
+        />
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
+              <DialogPanel
+                transition
+                className="pointer-events-auto relative w-screen max-w-md transform transition duration-500 ease-in-out data-closed:translate-x-full sm:duration-700"
+              >
+                <TransitionChild>
+                  <div className="absolute top-0 left-0 -ml-8 flex pt-4 pr-2 duration-500 ease-in-out data-closed:opacity-0 sm:-ml-10 sm:pr-4">
+                    <button
+                      type="button"
+                      onClick={() => setOpen(false)}
+                      className="relative rounded-md text-gray-300 hover:text-white focus-visible:ring-2 focus-visible:ring-white focus-visible:outline-hidden"
+                    >
+                      <span className="absolute -inset-2.5" />
+                      <span className="sr-only">Close panel</span>
+                      <XMarkIcon aria-hidden="true" className="size-6" />
+                    </button>
+                  </div>
+                </TransitionChild>
+                <div className="flex h-full flex-col overflow-y-auto bg-black py-6 shadow-xl">
+                  {/* <div className="px-4 sm:px-6">
+                    <DialogTitle className="text-base font-semibold text-white">
+                      Navigation
+                    </DialogTitle>
+                  </div> */}
+                  <div className="h-full relative mt-6 flex flex-col justify-between px-4 sm:px-6">
+                    <div className="flex flex-col gap-5 items-center">
+                      {props.navs.map((text) => (
+                        <MobileNavButton
+                          key={text}
+                          text={text}
+                          setOpen={setOpen}
+                        />
+                      ))}
+                    </div>
+                    <div className="flex justify-center">
+                      <a
+                        href="/blog"
+                        className="flex justify-center items-center text-yellow-300 bg-zinc-800 w-64 h-12 rounded hover:text-white"
+                      >
+                        Blog
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </DialogPanel>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </>
+  );
+}
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import MobileNav from "./MobileNav";
+
 interface ButtonProps {
   text: string;
 }
@@ -29,22 +31,27 @@ const NavButton = (props: ButtonProps) => {
 };
 
 export default function NavBar() {
+  const nav = ["About Me", "Education", "Experience", "Projects", "Contact Me"];
+
   return (
-    <div className="fixed flex flex-col w-36 h-full rounded-r-lg bg-black space-y-5 pt-5 invisible md:visible">
-      <div className="grow flex flex-col space-y-5">
-        <NavButton text="About Me" />
-        <NavButton text="Education" />
-        <NavButton text="Experience" />
-        <NavButton text="Projects" />
-        <NavButton text="Contact Me" />
+    <div>
+      <div className="fixed flex flex-col w-36 h-full rounded-r-lg bg-black space-y-5 pt-5 invisible md:visible">
+        <div className="grow flex flex-col space-y-5">
+          {nav.map((text) => (
+            <NavButton key={text} text={text} />
+          ))}
+        </div>
+        <div className="py-10">
+          <a
+            href="/blog"
+            className="flex justify-center items-center text-yellow-300 bg-zinc-800 w-32 h-8 rounded-e hover:text-white"
+          >
+            Blog
+          </a>
+        </div>
       </div>
-      <div className="py-10">
-        <a
-          href="/blog"
-          className="flex justify-center items-center text-yellow-300 bg-zinc-800 w-32 h-8 rounded-e hover:text-white"
-        >
-          Blog
-        </a>
+      <div className="fixed top-0 right-0 visible md:invisible justify-items-end bg-zinc-900 rounded-lg">
+        <MobileNav navs={nav} />
       </div>
     </div>
   );
