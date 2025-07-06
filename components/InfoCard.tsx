09202d913f4408a16dea2ac2dd7c3940a883c89e
Kory <74568018+lopezkor000@users.noreply.github.com>
2025-07-06 16:26:35 -0500

    styling

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -11,11 +11,13 @@ export default function InfoCard(props: Props) {
   return (
     <div
       id={props.id}
-      className="flex flex-col md:flex-row gap-10 items-start pb-10 w-full"
+      className="flex flex-col xl:flex-row gap-10 items-center pb-10 w-full px-3"
     >
       <img src={props.pic} className="size-60 aspect-square rounded-lg" />
       <div className="flex flex-col space-y-3">
-        <p className="text-2xl font-bold">{props.title}</p>
+        <p className="text-2xl font-bold text-center md:text-left">
+          {props.title}
+        </p>
         <p className="text-zinc-400">{props.subtitle}</p>
         <div className="space-y-5 text-base/7">{props.children}</div>
       </div>
